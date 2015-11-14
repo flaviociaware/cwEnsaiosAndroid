75117ac9cfc103f5608c5ff85a0b5c5f4d5fc7bc
Flavio Barbosa <flavio@ciaware.com.br>
2015-11-14 06:23:10 -0200

    ThiengoCalopsita - Parte 1

---
+++ ThiengoCalopsita/app/src/main/java/br/com/ciaware/android/ensaios/thiengocalopsita/MainActivity.java
@@ -0,0 +1,91 @@
+package br.com.ciaware.android.ensaios.thiengocalopsita;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Toolbar mToolbar;
+    private Toolbar mToolbarBottom;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mToolbar = (Toolbar)findViewById(R.id.tb_main);
+        mToolbar.setTitle("MainActivity");
+        mToolbar.setSubtitle("Sub título");
+        mToolbar.setLogo(R.drawable.ic_cw_people);
+        setSupportActionBar(mToolbar);
+
+        mToolbarBottom = (Toolbar)findViewById(R.id.inc_tb_bottom);
+
+        mToolbarBottom.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                Intent it = null;
+
+                switch(item.getItemId()){
+                    case R.id.action_facebook:
+                        it = new Intent(Intent.ACTION_VIEW);
+                        it.setData(Uri.parse("http://www.facebook.com"));
+                        break;
+                    case R.id.action_youtube:
+                        it = new Intent(Intent.ACTION_VIEW);
+                        it.setData(Uri.parse("http://www.youtube.com"));
+                        break;
+                    case R.id.action_google_plus:
+                        it = new Intent(Intent.ACTION_VIEW);
+                        it.setData(Uri.parse("http://plus.google.com"));
+                        break;
+                    case R.id.action_linkedin:
+                        it = new Intent(Intent.ACTION_VIEW);
+                        it.setData(Uri.parse("http://www.linkedin.com"));
+                        break;
+                    case R.id.action_whatsapp:
+                        it = new Intent(Intent.ACTION_VIEW);
+                        it.setData(Uri.parse("http://www.whatsapp.com"));
+                        break;
+                }
+
+                startActivity(it);
+                return true;
+            }
+        });
+
+        mToolbarBottom.inflateMenu(R.menu.menu_bottom);
+
+        mToolbarBottom.findViewById(R.id.iv_settings).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Settings pressed", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if(id == R.id.action_second_activity){
+            startActivity(new Intent(this, SecondActivity.class));
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}

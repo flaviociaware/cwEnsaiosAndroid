cf7733d26208a894a8bbd9ab6b62b6e9bdb6ca38
Flavio Barbosa <flavio@ciaware.com.br>
2015-11-15 17:52:43 -0200

    RecyclerView

--- ThiengoCalopsita/app/src/main/java/br/com/ciaware/android/ensaios/thiengocalopsita/MainActivity.java
+++ ThiengoCalopsita/app/src/main/java/br/com/ciaware/android/ensaios/thiengocalopsita/MainActivity.java
@@ -3,12 +3,19 @@ package br.com.ciaware.android.ensaios.thiengocalopsita;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import br.com.ciaware.android.ensaios.thiengocalopsita.domain.Car;
+import br.com.ciaware.android.ensaios.thiengocalopsita.fragments.CarFragment;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -23,7 +30,7 @@ public class MainActivity extends AppCompatActivity {
         mToolbar = (Toolbar)findViewById(R.id.tb_main);
         mToolbar.setTitle("MainActivity");
         mToolbar.setSubtitle("Sub título");
-        mToolbar.setLogo(R.drawable.ic_cw_people);
+        mToolbar.setLogo(R.drawable.ic_launcher);
         setSupportActionBar(mToolbar);
 
         mToolbarBottom = (Toolbar)findViewById(R.id.inc_tb_bottom);
@@ -70,6 +77,14 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        // FRAGMENT
+        CarFragment frag = (CarFragment) getSupportFragmentManager().findFragmentByTag("mainFrag");
+        if(frag == null) {
+            frag = new CarFragment();
+            FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+            ft.replace(R.id.rl_fragment_container, frag, "mainFrag");
+            ft.commit();
+        }
     }
 
     @Override
@@ -87,5 +102,18 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public List<Car> getSetCarList(int qtd){
+        String[] models = new String[]{"Gallardo", "Vyron", "Corvette", "Pagani Zonda", "Porsche 911 Carrera", "BMW 720i", "DB77", "Mustang", "Camaro", "CT6"};
+        String[] brands = new String[]{"Lamborghini", " bugatti", "Chevrolet", "Pagani", "Porsche", "BMW", "Aston Martin", "Ford", "Chevrolet", "Cadillac"};
+        int[] photos = new int[]{R.drawable.gallardo, R.drawable.vyron, R.drawable.corvette, R.drawable.paganni_zonda, R.drawable.porsche_911, R.drawable.bmw_720, R.drawable.db77, R.drawable.mustang, R.drawable.camaro, R.drawable.ct6};
+        List<Car> listAux = new ArrayList<>();
+
+        for(int i = 0; i < qtd; i++){
+            Car c = new Car( models[i % models.length], brands[ i % brands.length ], photos[i % models.length] );
+            listAux.add(c);
+        }
+        return(listAux);
     }
 }

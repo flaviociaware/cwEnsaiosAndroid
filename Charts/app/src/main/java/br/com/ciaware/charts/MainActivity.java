d3e7fb8ebb8445179b29e1191893dc56b582be8a
CW001 <notecw001@gmail.com>
2015-12-01 17:37:35 -0200

    Graficos de barra e linha

--- Charts/app/src/main/java/br/com/ciaware/charts/MainActivity.java
+++ Charts/app/src/main/java/br/com/ciaware/charts/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.charts.LineChart;
 import com.github.mikephil.charting.data.BarData;
 import com.github.mikephil.charting.data.BarDataSet;
 import com.github.mikephil.charting.data.BarEntry;
@@ -19,12 +20,23 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         BarChart chart = (BarChart) findViewById(R.id.bar_chart);
+        BarChart chart2 = (BarChart) findViewById(R.id.bar_chart2);
+
 
         BarData data = new BarData(getXAxisValues(), getDataSet());
         chart.setData(data);
         chart.setDescription("Grafico Barra");
         chart.animateXY(2000, 2000);
         chart.invalidate();
+
+        BarData data2 = new BarData(getXAxisValues2(), getDataSet2());
+        chart2.setData(data2);
+        chart2.setDescription("Grafico Barra");
+        chart2.animateXY(2000, 2000);
+        chart2.invalidate();
+
+
+
     }
 
     private ArrayList<BarDataSet> getDataSet() {
@@ -78,5 +90,59 @@ public class MainActivity extends AppCompatActivity {
         xAxis.add("MAY");
         xAxis.add("JUN");
         return xAxis;
+    }
+
+
+    private ArrayList<BarDataSet> getDataSet2() {
+        ArrayList<BarDataSet> dataSets = null;
+
+        ArrayList<BarEntry> valueSet1 = new ArrayList<>();
+        BarEntry v1e1 = new BarEntry(10.000f, 0); // Jan
+        valueSet1.add(v1e1);
+        BarEntry v1e2 = new BarEntry(400.000f, 1); // Feb
+        valueSet1.add(v1e2);
+        BarEntry v1e3 = new BarEntry(602.000f, 2); // Mar
+        valueSet1.add(v1e3);
+        BarEntry v1e4 = new BarEntry(310.000f, 3); // Apr
+        valueSet1.add(v1e4);
+        BarEntry v1e5 = new BarEntry(50.000f, 4); // May
+        valueSet1.add(v1e5);
+        BarEntry v1e6 = new BarEntry(10.000f, 5); // Jun
+        valueSet1.add(v1e6);
+
+        ArrayList<BarEntry> valueSet2 = new ArrayList<>();
+        BarEntry v2e1 = new BarEntry(15.000f, 0); // Jan
+        valueSet2.add(v2e1);
+        BarEntry v2e2 = new BarEntry(900.000f, 1); // Feb
+        valueSet2.add(v2e2);
+        BarEntry v2e3 = new BarEntry(1200.000f, 2); // Mar
+        valueSet2.add(v2e3);
+        BarEntry v2e4 = new BarEntry(110.000f, 3); // Apr
+        valueSet2.add(v2e4);
+        BarEntry v2e5 = new BarEntry(240.000f, 4); // May
+        valueSet2.add(v2e5);
+        BarEntry v2e6 = new BarEntry(18.000f, 5); // Jun
+        valueSet2.add(v2e6);
+
+        BarDataSet barDataSet3 = new BarDataSet(valueSet1, "Faixa SEMANA 1");
+        barDataSet3.setColor(Color.rgb(255,140,0));
+        BarDataSet barDataSet4 = new BarDataSet(valueSet2, "Faixa SEMANA 2");
+        barDataSet4.setColors(ColorTemplate.COLORFUL_COLORS);
+
+        dataSets = new ArrayList<>();
+        dataSets.add(barDataSet3);
+        dataSets.add(barDataSet4);
+        return dataSets;
+    }
+
+    private ArrayList<String> getXAxisValues2() {
+        ArrayList<String> xAxis2 = new ArrayList<>();
+        xAxis2.add("SEG");
+        xAxis2.add("TER");
+        xAxis2.add("QUAR");
+        xAxis2.add("QUIN");
+        xAxis2.add("SEX");
+        xAxis2.add("SAB");
+        return xAxis2;
     }
 }
